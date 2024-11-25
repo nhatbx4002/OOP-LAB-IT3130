64bfd47a064b3c7eaf132627e80d36b7cd1cb840
nhatbx4002 <nhatbui2004.work@gmail.com>
2024-11-25 16:58:56 +0700

    feature/print-cart

--- Lab3/Lab02/Code/AimsProjects/src/Cart.java
+++ Lab3/Lab02/Code/AimsProjects/src/Cart.java
@@ -6,6 +6,9 @@ public class Cart {
 	
 	private int qtyOrdered = 0; 
 	
+	public int getQty () {
+		return qtyOrdered; 
+	}
 	
 	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
 		if(qtyOrdered < MAX_NUMBERS_ORDERED) {
@@ -88,5 +91,33 @@ public class Cart {
 			System.out.println("Not found disc");
 		}
 	}
-			
+	//print cart 
+	public void printCart () {
+		float total = 0 ; 
+		System.out.println("***********************CART***********************");
+		for(int i = 0 ; i < qtyOrdered ; i++) {
+			System.out.println((i+1) + ". " + itemsOrdered[i].toString() );
+			total += itemsOrdered[i].getCost();
+		}
+		System.out.println("Total cost : " +  total);
+	}
+	
+	public void searchDVDInCart (String title ) {
+		for(int i = 0 ; i < qtyOrdered ; i++) {
+			if(itemsOrdered[i].isMatch(title)) {
+				System.out.println(itemsOrdered[i].toString());
+				return; 
+			}
+			System.out.println("Not found !");
+		}
+	}
+	
+	public void searchDVDInCart (int id) {
+		if(id < qtyOrdered && id > 0 ) {
+			System.out.println(itemsOrdered[id]);
+		}else {
+			System.out.print("Not found");
+		}
+	}
+	
 }
