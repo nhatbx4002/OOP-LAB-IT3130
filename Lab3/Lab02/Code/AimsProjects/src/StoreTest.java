b663f41239d72b2c18f379e119cfa146cd6af99f
nhatbx4002 <nhatbui2004.work@gmail.com>
2024-11-26 18:39:44 +0700

    class store updated

---
+++ Lab3/Lab02/Code/AimsProjects/src/StoreTest.java
@@ -0,0 +1,23 @@
+
+public class StoreTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Aller", 87, 19.95f);
+	        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+	        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.89f);
+
+	        // Create a store
+	        Store store = new Store();
+
+	        // Test adding DVDs
+	        store.addDVD(dvd1);
+	        store.addDVD(dvd2);
+	        store.addDVD(dvd3);
+
+	        // Test removing a DVD
+	        store.removeDVD("Star Wars");
+	        store.removeDVD("Frozen");
+	}
+
+}
